796af6d26ff0a674ae0189e3af12dbb690ac83f9
Shubham-Rasal <bluequbits@gmail.com>
2022-06-11 00:06:00 +0530

    Implemented ui for client view

--- frontend/project-manager/src/components/Clients.jsx
+++ frontend/project-manager/src/components/Clients.jsx
@@ -1,29 +1,47 @@
-import {gql,useQuery} from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
+import ClientRow from './ClientRow'
+
+import { GET_CLIENTS } from '../Queries/clientQueries';
 
 
-const GET_CLIENTS = gql`
-query getClients {
-  clients{
-    name,
-    id
-  }
-}
-`;
 
 const Clients = () => {
 
-    const {loading,error,data} = useQuery(GET_CLIENTS);
+    const { loading, error, data } = useQuery(GET_CLIENTS);
 
-    if(loading) return(<h1>Loading ...</h1>)
-    if(error) return(<h1>Something went Wrong</h1>)
-    
+    if (loading) return (
+    <div className="d-flex spinner-border text-dark" role="status">
+        <span className="visually-hidden"></span>
+    </div>
+    )
+    if (error) return (<h1>Something went Wrong</h1>)
 
-    console.log(data);
+
+    // console.log(data);
 
 
-  return (
-    <div>clients</div>
-  )
+    return (
+        <div className='container'>
+            <h3>Clients</h3>
+            <table className='table table-striped' >
+                <thead>
+                    <tr>
+                        <th >Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.clients.map(client => (
+                        <ClientRow key={client.id} client={client} />
+
+                    ))}
+
+                </tbody>
+            </table>
+
+        </div>
+    )
 }
 
 export default Clients
